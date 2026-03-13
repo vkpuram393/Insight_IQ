d425cd9ad133ee8af3cd8a16cfa3f6c8144630cb
Krishna, Vamshi Tcs <Vamshi.Krishna2@cvshealth.com>
2026-03-13 00:00:48 -0500

    Update logger.py

--- core/logger.py
+++ core/logger.py
@@ -5,7 +5,7 @@ import sys
 def get_logger(name: str) -> logging.Logger:
     logger = logging.getLogger(name)
     if not logger.handlers:
-        logger.setLevel(logging.DEBUG)
+        logger.setLevel(logging.ERROR)
         handler = logging.StreamHandler(sys.stdout)
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         handler.setFormatter(formatter)
